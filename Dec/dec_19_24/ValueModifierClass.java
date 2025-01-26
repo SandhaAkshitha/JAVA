d5acbf6245eaacf2ded32a8e9f2eee99374d25bb
SandhaAkshitha <sandhaakshitha@gmail.com>
2025-01-26 15:37:10 +0530

    Update ValueModifierClass.java

--- Dec/dec_19_24/ValueModifierClass.java
+++ Dec/dec_19_24/ValueModifierClass.java
@@ -1,1 +1,26 @@
 
+package dec_19_24;
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class ValueModifierclass
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println(" enter a String :");
+		String s=sc.nextLine();
+		
+		System.out.println(" enter a number: ");
+		int num=sc.nextInt();
+		
+		Consumer<String> c1=(String str) ->System.out.println("Ater converting string to uppercase:"+str.toUpperCase());
+	c1.accept(s);
+	Consumer<Integer> c2=(Integer)->System.out.println("Squares in the integer value: "+num*num);
+	c2.accept(num);
+	sc.close();
+	}
+
+}
