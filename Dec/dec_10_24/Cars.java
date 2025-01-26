5423a6d3a5bdc11cebf1b9452f8cbf82ff793b32
SandhaAkshitha <sandhaakshitha@gmail.com>
2025-01-26 15:04:05 +0530

    Update Cars.java

--- Dec/dec_10_24/Cars.java
+++ Dec/dec_10_24/Cars.java
@@ -1,1 +1,57 @@
 
+package dec_10_24;
+
+
+abstract class Cars {	
+		  String  brand;
+		  String  model;
+		  int mileage;
+		  int top_speed;
+		  int yearOfManufacturing;
+		  public Cars()
+		  {	 
+		  }
+		  public String carBrand()
+		  {
+			  return brand;
+		  }
+		  public String carModel()
+		  {
+			  return model;
+		  }
+		  public int carMileage()
+		  {
+			  return mileage;
+		  }
+		  public int carTopSpeed()
+		  {
+			  return top_speed;
+		  }
+		  public int carYear()
+		  {
+			  return yearOfManufacturing;
+		  }
+		  abstract public void display();
+}
+class Test extends Cars{
+	@Override
+   public  void display()
+    {
+		System.out.println("Car brand :"+brand);
+		System.out.println("Car model :"+model);
+		System.out.println("Car mileage :"+mileage);
+		System.out.println("Car top_speed :"+top_speed);
+		System.out.println("Car yearOfManufacturing :"+yearOfManufacturing);
+    }
+}
+public class Car1{
+	public static void main(String[] args) {
+		Cars car = new Test();
+		car.brand="BMW";
+		car.model="micro";
+		car.mileage=56;
+		car.top_speed=20;
+		car.yearOfManufacturing=2024;
+		car.display();
+	}
+}
