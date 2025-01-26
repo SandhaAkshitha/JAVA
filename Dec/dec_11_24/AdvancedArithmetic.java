c436ff49c87e5fa05b9e736b8f9613cac54c298b
SandhaAkshitha <sandhaakshitha@gmail.com>
2025-01-26 15:17:23 +0530

    Update AdvancedArithmetic.java

--- Dec/dec_11_24/AdvancedArithmetic.java
+++ Dec/dec_11_24/AdvancedArithmetic.java
@@ -1,1 +1,35 @@
 
+package dec_11_24;
+
+interface AdvancedArithmetic1 
+{
+	public abstract int divisorSum(int n);
+}
+class MyCalculator implements AdvancedArithmetic1
+{
+    @Override
+	public int divisorSum(int n) {
+		int sum=0;
+		if( n <= 0)
+		{
+			System.err.println("Error : input must be postive");
+		}
+		for(int i =1; i<=n;i++)
+		{
+			if(n % i==0)
+			{
+				sum = sum+i;
+			}	
+		}	
+		return sum;
+	}	
+}
+public class AdvancedArithmetic
+{
+	public static void main(String[] args) {
+		MyCalculator a = new MyCalculator();
+		int res=a.divisorSum(6);
+		System.out.println("Total divisor sum:"+res);
+	}
+}
+
