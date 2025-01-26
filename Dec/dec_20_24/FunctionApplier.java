4cbb9b478fee732cb58769ef982cd1e712a15a70
SandhaAkshitha <sandhaakshitha@gmail.com>
2025-01-26 15:43:25 +0530

    Update FunctionApplier.java

--- Dec/dec_20_24/FunctionApplier.java
+++ Dec/dec_20_24/FunctionApplier.java
@@ -1,1 +1,36 @@
 
+package dec_20_24;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class FunctionApplier 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter Number: ");
+		int number = sc.nextInt();
+		
+		Function<Integer, Integer> add=num-> number+num;
+		System.out.println("After adding 5: "+applyFunction(5, add));
+		
+		Function<Integer,Integer> multiply = num -> number * num;
+		System.out.println("After multiplying by 2: "+applyFunction(2, multiply));
+		
+		Function<Integer,Integer> sub = num -> number - num;
+		System.out.println("After subtracting 3: "+applyFunction(3, sub));
+	
+	sc.close();
+	}
+	public static int applyFunction(int value,  Function<Integer, Integer> function)
+	
+	{
+		return function.apply(value);
+	}
+		
+		
+	}
